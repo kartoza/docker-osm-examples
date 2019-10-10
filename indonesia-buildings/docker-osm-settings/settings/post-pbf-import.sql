4be4901694c6646b40a9bcf45276f357cf70f058
admire <admire@kartoza.com>
2019-10-10 16:55:09 +0200

    Fix database SQL

--- indonesia-buildings/docker-osm-settings/settings/post-pbf-import.sql
+++ indonesia-buildings/docker-osm-settings/settings/post-pbf-import.sql
@@ -1,10 +1,40 @@
+-- Add a trigger function to notify QGIS of DB changes
+CREATE FUNCTION public.notify_qgis() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+        BEGIN NOTIFY qgis;
+        RETURN NULL;
+        END;
+    $$;
+
+-- Based on the tables defined in the mapping.yml create triggers
+
+CREATE TRIGGER notify_admin
+  AFTER INSERT OR UPDATE OR DELETE  ON public.osm_admin
+    FOR EACH STATEMENT EXECUTE PROCEDURE public.notify_qgis();
+
+    CREATE TRIGGER notify_buildings
+  AFTER INSERT OR UPDATE OR DELETE  ON public.osm_buildings
+    FOR EACH STATEMENT EXECUTE PROCEDURE public.notify_qgis();
+
+
+    CREATE TRIGGER notify_roads
+  AFTER INSERT OR UPDATE OR DELETE  ON public.osm_roads
+    FOR EACH STATEMENT EXECUTE PROCEDURE public.notify_qgis();
+
+    CREATE TRIGGER notify_waterways
+  AFTER INSERT OR UPDATE OR DELETE  ON public.osm_waterways
+    FOR EACH STATEMENT EXECUTE PROCEDURE public.notify_qgis();
+
+-- Vulnerability reporting and mapping for Buildings
+
 ALTER table osm_buildings add column building_type character varying (100);
 
 
 CREATE OR REPLACE FUNCTION building_types_mapper () RETURNS trigger LANGUAGE plpgsql
 AS $$
 BEGIN
-    new.building_type :=
+    SELECT
     CASE
            WHEN new.amenity ILIKE '%school%' OR new.amenity ILIKE '%kindergarten%' THEN 'School'
            WHEN new.amenity ILIKE '%university%' OR new.amenity ILIKE '%college%' THEN 'University/College'
@@ -35,43 +65,19 @@ BEGIN
            -- Add default type
            WHEN new."type" IS NULL THEN 'Residential'
         END
+    INTO new.building_type
+    FROM osm_buildings
     ;
   RETURN NEW;
   END
   $$;
 
-CREATE TRIGGER types_mapper BEFORE INSERT OR UPDATE ON osm_buildings FOR EACH ROW EXECUTE PROCEDURE
+CREATE TRIGGER building_type_mapper BEFORE INSERT OR UPDATE ON osm_buildings FOR EACH ROW EXECUTE PROCEDURE
     building_types_mapper ();
 
--- Add a trigger function to notify QGIS of DB changes
-CREATE FUNCTION public.notify_qgis() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$
-        BEGIN NOTIFY qgis;
-        RETURN NULL;
-        END;
-    $$;
-
--- Based on the tables defined in the mapping.yml create triggers
-
-CREATE TRIGGER notify_admin
-  AFTER INSERT OR UPDATE OR DELETE  ON public.osm_admin
-    FOR EACH STATEMENT EXECUTE PROCEDURE public.notify_qgis();
-
-    CREATE TRIGGER notify_buildings
-  AFTER INSERT OR UPDATE OR DELETE  ON public.osm_buildings
-    FOR EACH STATEMENT EXECUTE PROCEDURE public.notify_qgis();
 
 
-    CREATE TRIGGER notify_roads
-  AFTER INSERT OR UPDATE OR DELETE  ON public.osm_roads
-    FOR EACH STATEMENT EXECUTE PROCEDURE public.notify_qgis();
-
-    CREATE TRIGGER notify_waterways
-  AFTER INSERT OR UPDATE OR DELETE  ON public.osm_waterways
-    FOR EACH STATEMENT EXECUTE PROCEDURE public.notify_qgis();
-
--- Create columns to use to keep recoded values
+-- Function to recode buildings that have been reclassified above using the function building_types_mapper ()
 ALTER table osm_buildings add column building_type_recode numeric;
 
 CREATE OR REPLACE FUNCTION building_recode_mapper () RETURNS trigger LANGUAGE plpgsql
@@ -99,10 +105,10 @@ BEGIN
   END
   $$;
 
-CREATE TRIGGER type_recode BEFORE INSERT OR UPDATE ON osm_buildings FOR EACH ROW EXECUTE PROCEDURE
+CREATE TRIGGER building_type_recoder BEFORE INSERT OR UPDATE ON osm_buildings FOR EACH ROW EXECUTE PROCEDURE
     building_recode_mapper();
 
--- Create a column to hold the calculated area in the table
+-- Create a column to hold the recoded calculated area in the table
 ALTER table osm_buildings add column area_recode numeric;
 
 CREATE OR REPLACE FUNCTION building_area_mapper () RETURNS trigger LANGUAGE plpgsql
@@ -127,9 +133,9 @@ CREATE TRIGGER area_recode_mapper BEFORE INSERT OR UPDATE ON osm_buildings FOR E
     building_area_mapper();
 
 -- Create a column to hold the building materials class in the table
-ALTER table osm_buildings add column walls_recode numeric;
+ALTER table osm_buildings add column building_material_recode numeric;
 
-CREATE OR REPLACE FUNCTION building_walls_mapper () RETURNS trigger LANGUAGE plpgsql
+CREATE OR REPLACE FUNCTION building_materials_mapper () RETURNS trigger LANGUAGE plpgsql
 AS $$
 BEGIN
     SELECT
@@ -139,17 +145,17 @@ BEGIN
         WHEN new."building:material" = 'glass' THEN 0.3
         ELSE 0.3
         END
-    INTO walls_recode
+    INTO new.building_material_recode
     FROM osm_buildings
     ;
   RETURN NEW;
   END
   $$;
 
-CREATE TRIGGER walls_mapper BEFORE INSERT OR UPDATE ON osm_buildings FOR EACH ROW EXECUTE PROCEDURE
-    building_walls_mapper();
+CREATE TRIGGER building_material_mapper BEFORE INSERT OR UPDATE ON osm_buildings FOR EACH ROW EXECUTE PROCEDURE
+    building_materials_mapper();
 
--- Create a column to hold the centroid distance to the nearest river
+-- Function to calculate the distance from a river to the centroid of the building
 ALTER table osm_buildings add column river_distance numeric;
 
 CREATE OR REPLACE FUNCTION river_distance_mapper () RETURNS trigger LANGUAGE plpgsql
@@ -177,9 +183,9 @@ AS $$
 BEGIN
     SELECT
         CASE
-            WHEN new.river_distance > 0 and <= 100 THEN 1.0
-            WHEN new.river_distance > 100 and <= 300  THEN 0.7
-            WHEN new.river_distance > 300 and <= 500  THEN 0.5
+            WHEN new.river_distance > 0 and new.river_distance <= 100 THEN 1.0
+            WHEN new.river_distance > 100 and new.river_distance <= 300  THEN 0.7
+            WHEN new.river_distance > 300 and new.river_distance <= 500  THEN 0.5
             WHEN new.river_distance > 500 THEN 0.3
             ELSE 0.3
         END
@@ -196,6 +202,7 @@ CREATE TRIGGER river_recode_mapper BEFORE INSERT OR UPDATE ON osm_buildings FOR 
 
 
 -- count number or roads intersecting Surabaya
+CREATE VIEW osm_roads_surabaya_stats as
 SELECT type, COUNT(osm_id) FROM (
     SELECT DISTINCT ON (a.osm_id) a.osm_id, a.type
     FROM osm_roads as a
@@ -205,3 +212,19 @@ GROUP BY type order by count;
 
 
 -- count number of rivers intersecting surabaya
+CREATE OR REPLACE VIEW osm_rivers_surabaya_stats as
+SELECT type, COUNT(osm_id) FROM (
+    SELECT DISTINCT ON (a.osm_id) a.osm_id, a.waterway
+    FROM osm_waterways as a
+    INNER JOIN osm_admin as b ON ST_Intersects(a.geometry, b.geometry) where b.name = 'Surabaya'
+) subquery
+GROUP BY type order by count;
+
+-- count number of buildings intersecting surabaya
+CREATE OR REPLACE VIEW osm_buildings_surabaya_stats as
+SELECT type, COUNT(osm_id) FROM (
+    SELECT DISTINCT ON (a.osm_id) a.osm_id, a.building_type
+    FROM osm_buildings as a
+    INNER JOIN osm_admin as b ON ST_Intersects(a.geometry, b.geometry) where b.name = 'Surabaya'
+) subquery
+GROUP BY type order by count;
